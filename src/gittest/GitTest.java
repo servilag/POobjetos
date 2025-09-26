7683084eb6c734d9e651b4ff37f1f04e100ea92c
CENTIC <CENTIC@10.6.65.240>
2025-09-26 10:26:31 -0500

    primera modificaciao

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -15,6 +15,8 @@ public class GitTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("AWEBO");
     }
     
 }
