4d30ce7ba981b9f2c96d20fe26ce33c0784f3211
CENTIC <CENTIC@10.6.65.220>
2025-09-26 12:39:35 -0500

    ...

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -1,8 +1,27 @@
 package gittest;
 
+import gittest.Booking.*;
+import gittest.Customers.*;
+
 public class GitTest {
 
     public static void main(String[] args) {
+        
+        try{
+            Bus trans1 = new Bus(123, "Dubai" , "Bahamas", 12.99, "LimBusCompany" , "P3");
+            Flight trans2 = new Flight(456, "Miami", "Mexico", 3.99, "Avianca", "E3T");
+            Train trans3= new Train(789, "Bogota", "cali", 60.99, "Metrolinea", 8); 
+            
+            System.out.println("TRANSPORTS AVAILABLE");
+            System.out.println(trans1.toString());
+            System.out.println(trans2.toString());
+            System.out.println(trans3.toString());
+              
+            
+       }catch(Exception e){
+            System.out.println("You are taking too long " + e.getMessage());
+           
+       }
         
         
         
