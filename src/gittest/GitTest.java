283c3b09ce8e25f783233d7ebe77439465377c42
CENTIC <CENTIC@10.6.65.240>
2025-09-26 11:03:09 -0500

    Empezando

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -1,22 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gittest;
 
-/**
- *
- * @author CENTIC
- */
 public class GitTest {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        System.out.println("AWEBO");
+        
+        
     }
     
 }
