ba7ebff30ddb2a64cf8056087ac8b89f1ac95461
CENTIC <CENTIC@10.6.65.240>
2025-09-26 12:09:09 -0500

    resto de clases

---
+++ src/gittest/Booking/Bus.java
@@ -0,0 +1,41 @@
+package gittest.Booking;
+
+public class Bus extends TransportService{
+    private String BusCompany;
+    private String BoardingPoint;
+    
+    public Bus (){}
+    
+    public Bus(int id, String origin, String destination, double price, String BusCompany, String BoardingPoint){
+        super(id,origin,destination,price);
+        this.setBusCompany(BusCompany);
+        this.setBoardingPoint(BoardingPoint);
+    }
+    public void setBusCompany(String SeatType){
+        this.BusCompany=SeatType;
+    }
+    public void setBoardingPoint(String BoardingPoint){
+        this.BoardingPoint=BoardingPoint;
+    }
+    public String getBusCompany(){
+        return this.BusCompany;
+    }
+    public String getBoardingPoint(){
+        return this.BoardingPoint;
+    }
+    @Override
+    public String getServiceInformation(){
+        String str="on tracks";
+        return str;
+    }
+    @Override
+    public String getLuggageInformation(){
+        String str="Half full";
+        return str;
+    }
+     @Override
+     public String toString(){
+         String str= super.toString() +"\nBusCompany:\t"+this.getBusCompany()+"\nBoarding Point:\t"+this.getBoardingPoint();
+         return str;
+     }
+}
